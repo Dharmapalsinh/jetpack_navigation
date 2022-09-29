7b4b8d46b805a70e412272f0f3579dde0aefa618
Dharmapalsinh <DharmapalsinhVaghela99099@gmail.com>
2022-09-29 14:34:09 +0530

    popup added.

--- app/src/main/java/com/dharmapal/jetpack_navigation/Adapter/MoviesAdapter.kt
+++ app/src/main/java/com/dharmapal/jetpack_navigation/Adapter/MoviesAdapter.kt
@@ -1,8 +1,9 @@
 
 
 import android.graphics.Bitmap
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -11,10 +12,13 @@ import com.dharmapal.jetpack_navigation.Models.MovieResult
 import com.dharmapal.jetpack_navigation.Models.Movies
 import com.dharmapal.jetpack_navigation.Models.VideoResult
 import com.dharmapal.jetpack_navigation.Models.diff
+import com.dharmapal.jetpack_navigation.R
 import com.dharmapal.jetpack_navigation.databinding.MovieRowBinding
+import java.nio.file.Files.delete
 
-class MoviesAdapter(val onClick:(VideoResult)->Unit): ListAdapter<VideoResult,myViewHolder>(diff) {
+class MoviesAdapter(val onClick:(VideoResult)->Unit,val onwatchclick:(VideoResult)->Unit): RecyclerView.Adapter<myViewHolder>() {
 
+    var data:List<VideoResult> = emptyList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
         val inflater= LayoutInflater.from(parent.context)
         val binding= MovieRowBinding.inflate(inflater,parent,false)
@@ -22,17 +26,42 @@ class MoviesAdapter(val onClick:(VideoResult)->Unit): ListAdapter<VideoResult,my
     }
 
     override fun onBindViewHolder(holder: myViewHolder, position: Int) {
-        val b1:VideoResult=getItem(position)
+        val b1:VideoResult=data[position]
         Glide.with(holder.itemView.context).load(b1.thumbnail!!.static).into(holder.binding.ivUserImage)
         holder.binding.tvTitle.text=b1.title
         holder.itemView.setOnClickListener {
-            onClick(b1)
+//            onClick(b1)
+            val popup = PopupMenu(holder.itemView.context,holder.binding.tvTitle)
+            popup.inflate(R.menu.menufilecontext)
+            popup.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem): Boolean {
+                    when (item.getItemId()) {
+                        R.id.Watch ->{
+                            onwatchclick(b1)
+                            return true
+                        }
+                        R.id.View->{
+                            onClick(b1)
+                            return true
+                        }
+                        else -> return false
+                    }
+                }
+            })
+            popup.show()
         }
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
     }
 }
 
 
-class myViewHolder(val binding: MovieRowBinding): RecyclerView.ViewHolder(binding.root)
+class myViewHolder(val binding: MovieRowBinding): RecyclerView.ViewHolder(binding.root){
+
+}
 
 
 
--- app/src/main/java/com/dharmapal/jetpack_navigation/homeFragment.kt
+++ app/src/main/java/com/dharmapal/jetpack_navigation/homeFragment.kt
@@ -1,43 +1,45 @@
 package com.dharmapal.jetpack_navigation
 
 import MoviesAdapter
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.dharmapal.gatetouch_task.Retrofit.RetrofitService
 import com.dharmapal.gatetouch_task.Retrofit.RetrofitService.Companion.retrofitService
 import com.dharmapal.jetpack_navigation.databinding.FragmentHomeBinding
+import java.nio.file.Files.delete
 
 class homeFragment : Fragment() {
 
     private lateinit var viewmodel: MainViewmodel
     private val retrofitService = RetrofitService.getInstance()
     private lateinit var binding:FragmentHomeBinding
-    private val adapter=MoviesAdapter{
+    private val adapter=MoviesAdapter(onClick = {
         findNavController().navigate(homeFragmentDirections.actionHomeFragmentToMovieDetailFragment(it.title!!))
-    }
+    }, onwatchclick = {startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(it.link)))})
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding=FragmentHomeBinding.inflate(inflater)
-
         val viewModelFactory=MainViewmodelFactory(Repo(retrofitService))
         viewmodel= ViewModelProvider(this,viewModelFactory)[MainViewmodel::class.java]
 
         viewmodel.getmovies()
         viewmodel.movielist.observe(viewLifecycleOwner){
-            adapter.submitList(it)
+            adapter.data=(it)
             binding.recycleview.adapter=adapter
         }
         return binding.root
     }
+
+
 
 }
